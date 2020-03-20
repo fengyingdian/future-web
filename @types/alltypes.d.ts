ac41bd67948ea37fba793ed90812b6ffd0026eba
break <fengyingdian@126.com>
2020-03-20 12:12:32 +0800

    add a test

---
+++ @types/alltypes.d.ts
@@ -0,0 +1,2 @@
+declare module 'react-window';
+declare module 'react-columns';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import NavButton from '../src/components/NavButton';
+// import Typography from '@material-ui/core/Typography';
+// import NavButton from '../src/components/NavButton';
 import useStyles from '../src/model/styles';
+import FlexColumn from '../src/components/FlexColumns/index';
+// import Card from '../src/components/Card/index';
+// import VirtualizedList from '../src/components/List';
 
 export const Index = () => {
 	const classes = useStyles();
 
 	return (
 		<Container maxWidth={false} className={classes.root}>
-			<Typography variant={'h4'} component={'h1'} gutterBottom className={classes.titleLarge}>
+			{/* <Typography variant={'h4'} component={'h1'} gutterBottom className={classes.titleLarge}>
 				{'Guide'}
-			</Typography>
-			<NavButton link={'/openlink'} title={'OpenLink'} />
+			</Typography> */}
+			{/* <NavButton link={'/openlink'} title={'OpenLink'} />
 			<NavButton link={'/edit'} title={'Edit News'} />
 			<NavButton link={'/quote'} title={'Quote From Link'} />
-			<NavButton link={'/preview'} title={'Preview News'} />
+			<NavButton link={'/preview'} title={'Preview News'} /> */}
+			<FlexColumn />
+			{/* <Card /> */}
+			{/* <VirtualizedList /> */}
 		</Container>
 	);
 };
