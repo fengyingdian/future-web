ccee8208021fa8c0d3f49bac8ee301d808a45414
break <fengyingdian@126.com>
2020-04-07 21:45:51 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: {
-		fabulousService: 'http://fabulous.flipboard.cn/api/web',
+		fabulousService: 'https://fabulous.flipboard.cn/api/web',
 		// fabulousService: 'http://localhost:8131313/api/web',
 	},
 };
